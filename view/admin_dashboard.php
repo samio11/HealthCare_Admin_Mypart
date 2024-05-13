b4a5fc182bcfbf17f95182cd4987ccf5dfb68fc8
Samio Hasan <138763106+samio11@users.noreply.github.com>
2024-05-13 10:37:51 +0600

    JQuery Part Done

--- view/admin_dashboard.php
+++ view/admin_dashboard.php
@@ -6,7 +6,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="./css/admin_dashbord.css">
     <title>Admin DashBoard</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
 </head>
+<script>
+    $(document).ready(function() {
+        $("#search-button").click(function() {
+            $("#txt1").fadeToggle("slow");
+        });
+    });
+</script>
 
 <body>
     <?php
